2e664613bde26119663f5b4da1f50a266939264e
Jan Van den bosch <jan.van.den.bosch@devoteam.com>
2016-05-03 09:04:04 +0200

    added similar operations alongside count

--- src/main/scala/TilePuzzle.scala
+++ src/main/scala/TilePuzzle.scala
@@ -8,6 +8,55 @@ object TilePuzzle extends App {
     count0(grid occupy startPos, startPos)
   }
 
+  def alternativeCount(grid: Grid, startPos: Position): Int = {
+    def count0(grid: Grid, currentPos: Position): Int = {
+      val next = grid.occupy(currentPos)
+
+      if (next.solved) 1
+      else next.nextPositions(currentPos).foldLeft(0)((sum, nextPos) => sum + count0(next, nextPos))
+    }
+
+    count0(grid, startPos)
+  }
+
+  def solvable(grid: Grid, startPos: Position): Boolean = {
+    def solvable0(grid: Grid, currentPos: Position): Boolean = {
+      val moved = grid.occupy(currentPos)
+
+      if (moved.solved) true
+      else moved.nextPositions(currentPos).exists(nextPos => solvable0(moved, nextPos))
+    }
+
+    solvable0(grid, startPos)
+  }
+
+  def paths(grid: Grid, startPos: Position): Iterable[Iterable[Position]] = {
+    def paths0(grid: Grid, currentPos: Position, path: Vector[Position]): Iterable[Iterable[Position]] = {
+      if (grid.solved) Vector(path)
+      else grid.nextPositions(currentPos).foldLeft(Vector[Iterable[Position]]())((acc, nextPos) => acc ++ paths0(grid occupy nextPos, nextPos, path :+ nextPos))
+    }
+
+    paths0(grid occupy startPos, startPos, Vector(startPos))
+  }
+
+  def path(grid: Grid, startPos: Position): Option[Iterable[Position]] = {
+    def path0(grid: Grid, currentPos: Position, path: Vector[Position]): Option[Iterable[Position]] = {
+      if (grid.solved) Some(path)
+      else {
+        grid.nextPositions(currentPos).foreach(nextPos => {
+          path0(grid occupy nextPos, nextPos, path :+ nextPos) match {
+            case path: Some[_] => return path // return early if found
+            case _ =>
+          }
+        })
+
+        None
+      }
+    }
+
+    path0(grid occupy startPos, startPos, Vector(startPos))
+  }
+
   // http://faqsmedia.ign.com/faqs/image/zelda_oracle_of_ages_tile_3.gif
   val startPos = Position(6, 1)
   val occupieds: Iterable[Position] = List(
@@ -23,5 +72,12 @@ object TilePuzzle extends App {
   })
 
   println(grid)
-  println(count(grid, startPos))
+
+  println(new java.util.Date)
+
+  val solutions = paths(grid, startPos)
+  println("solutions: " + solutions.size)
+  solutions.take(3) foreach println
+
+  println(new java.util.Date)
 }
